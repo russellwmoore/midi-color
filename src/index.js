36007f1c7db3f39e5aef37b2da0d2e34b94ad66e
Michael Caterisano <mcaterisano@gmail.com>
2020-05-07 17:11:26 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { add } from "./whatever";
+
+const heyMan = (name) => {
+  console.log(name);
+  return name;
+};
+
+const result = add(1, 2, 3);
+console.log(result);
+console.log("from index");
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development", // change to expression based on env variables
+  devtool: "source-map", // git ignore everything in dist
+  module: {
+    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }],
+  },
+};
