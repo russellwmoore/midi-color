fc0b0dfaf7cf9bb85b0205bb03413f9f5c190210
Michael Caterisano <mcaterisano@gmail.com>
2020-05-08 00:58:02 -0400

    working midi parse

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-import { add } from "./importMe";
+import { Midi } from "@tonejs/midi";
 
-const result = add(1, 5, 4);
-console.log(result);
+async function logMidi() {
+  const midi = await Midi.fromUrl("./midi/keys.mid");
+
+  const notes = midi.tracks[0].notes;
+
+  notes.map((note) => console.log(note));
+}
+
+logMidi();
