727de250b65b434ecddfecac5638852aa876488b
Michael Caterisano <mcaterisano@gmail.com>
2020-05-08 02:22:11 -0400

    schedule dom element append with midi note time

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import { Midi } from "@tonejs/midi";
+import { Transport } from "tone";
 
 async function logMidi() {
   const midi = await Midi.fromUrl("./midi/keys.mid");
 
   const notes = midi.tracks[0].notes;
 
-  notes.map((note) => console.log(note));
+  notes.map((note) => {
+    Transport.schedule(function (time) {
+      console.log(time);
+      const element = document.createElement("div");
+      element.innerText = "heyman from index";
+      element.style.background = "yellow";
+      document.body.appendChild(element);
+    }, note.time);
+  });
 }
 
-logMidi();
+function start(e) {
+  e.preventDefault();
+  Transport.start();
+  logMidi();
+}
+
+const startButton = document.querySelector("#start");
+startButton.addEventListener("click", start);
