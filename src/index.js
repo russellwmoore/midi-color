c87d564edb989fd3699dca474ff6d6ec81261479
Michael Caterisano <mcaterisano@gmail.com>
2020-05-08 16:01:24 -0400

    add stop button

--- src/index.js
+++ src/index.js
@@ -2,38 +2,46 @@ import { Midi } from "@tonejs/midi";
 import { Transport } from "tone";
 import { Player } from "tone";
 import { Buffer } from "tone";
+import { Draw } from "tone";
 
+// create start button
 const startButton = document.createElement("button");
 startButton.setAttribute("id", "start");
 startButton.innerText = "start";
 startButton.setAttribute("disabled", true);
+startButton.addEventListener("click", start);
 document.body.appendChild(startButton);
 
+// create stop button
+const stopButton = document.createElement("button");
+stopButton.setAttribute("id", "stop");
+stopButton.innerText = "stop";
+stopButton.addEventListener("click", stop);
+document.body.appendChild(stopButton);
+
+// create Tone.Player
 const player = new Player("./audio/keys.mp3", {
   autoplay: false,
 }).toMaster();
-
 player.sync().start();
 
-startButton.addEventListener("click", () => {
-  start();
-});
-
+// enable start button when audio file loaded
 Buffer.on("load", () => {
-  console.log("loaded");
   startButton.removeAttribute("disabled");
 });
 
+// append/remove elements from dom based on midi note times
 async function draw() {
+  // get midi data
   const midi = await Midi.fromUrl("./midi/keys.mid");
 
+  // get notes from first track
   const notes = midi.tracks[0].notes;
 
-  //
-  notes.map((note) => {
-    console.log(note);
-    Transport.schedule(function (time) {
-      console.log(time);
+  //loop through notes
+  notes.forEach((note) => {
+    // schedule append element based on note.time
+    Draw.schedule(function () {
       const element = document.createElement("div");
       element.setAttribute("id", "yellow-rectangle");
       element.style.width = "100%";
@@ -42,17 +50,24 @@ async function draw() {
       element.style.marginTop = "30px";
       document.body.appendChild(element);
 
-      Transport.schedule(function (endTime) {
+      // schedule remove element after note duration
+      Draw.schedule(function () {
         document.body.removeChild(element);
       }, note.time + note.duration);
     }, note.time);
   });
 }
 
+// start transport and trigger append elements
 function start() {
+  console.log("start");
   Transport.start();
   draw();
 }
 
-// const startButton = document.querySelector("#start");
-// startButton.addEventListener("click", start);
+// stop transport and cancel scheduled Draw events.
+// TODO: Figure out why Draw does not start from zero after stop
+function stop() {
+  Draw.cancel();
+  Transport.stop();
+}
