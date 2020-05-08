2e7d11b68f4a341252fa64f8934d0558b26946ab
Michael Caterisano <mcaterisano@gmail.com>
2020-05-08 02:55:20 -0400

    animate two yellow lines from midi file

--- src/index.js
+++ src/index.js
@@ -7,12 +7,20 @@ async function logMidi() {
   const notes = midi.tracks[0].notes;
 
   notes.map((note) => {
+    console.log(note);
     Transport.schedule(function (time) {
       console.log(time);
       const element = document.createElement("div");
-      element.innerText = "heyman from index";
+      element.setAttribute("id", "yellow-rectangle");
+      element.style.width = "100%";
+      element.style.height = "2px";
       element.style.background = "yellow";
+      element.style.marginTop = "30px";
       document.body.appendChild(element);
+
+      Transport.schedule(function (endTime) {
+        document.body.removeChild(element);
+      }, note.time + note.duration);
     }, note.time);
   });
 }
