9faacc8a225729791889182b00b74ad076b24314
Michael Caterisano <mcaterisano@gmail.com>
2020-05-13 00:32:23 -0400

    check for mobile

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import { Player } from "tone";
 import { Buffer } from "tone";
 import { Draw } from "tone";
 import { Transport } from "tone";
+import mobile from "is-mobile";
+
+console.log(mobile());
 
 // create start button
 const startButton = document.createElement("div");
+startButton.style.position = "absolute";
+startButton.style.top = 0;
 startButton.style.display = "flex";
 startButton.style.justifyContent = "center";
 startButton.style.alignItems = "center";
@@ -20,19 +25,20 @@ startButton.addEventListener("click", start);
 startButton.setAttribute("id", "start");
 document.body.appendChild(startButton);
 
+const height = mobile() ? window.innerHeight + "px" : "100vh";
+
 const innerGridStyle = `
   display: grid;
-  height: 70%;
+  height: 100%;
   width: 100%;
   grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
   grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
   box-sizing: border-box;
-  border: 1px solid white;
 `;
 
 const containerGridStyle = `
   display: grid;
-  height: 100vh;
+  height: ${height};
   width: 100vw;
   align-items: center;
 
@@ -48,6 +54,10 @@ const gridTwo = document.createElement("div");
 containerGrid.style = containerGridStyle;
 gridOne.style = innerGridStyle;
 gridTwo.style = innerGridStyle;
+
+document.body.appendChild(containerGrid);
+containerGrid.appendChild(gridTwo);
+containerGrid.appendChild(gridOne);
 
 // create Tone.Player
 const player = new Player("./audio/bachAudio.mp3", {
@@ -93,8 +103,11 @@ async function draw() {
     Transport.schedule(function (time) {
       Draw.schedule(function () {
         const element = document.createElement("div");
-        const column = 19 - Math.floor(scale(note.midi, 55, 72, 1, 18));
-        element.style.gridArea = `${column} / 1/ ${column} / 19`;
+        // const column = 19 - Math.floor(scale(note.midi, 55, 72, 1, 18));
+        // element.style.gridArea = `${column} / 1/ ${column} / 19`;
+
+        const row = Math.floor(scale(note.midi, 55, 72, 1, 18));
+        element.style.gridArea = `1 / ${row}/ 19 / ${row}`;
         element.style.zIndex = 5;
 
         const hue = scale(note.midi, 55, 72, 240, 300);
@@ -119,7 +132,6 @@ async function draw() {
       Draw.schedule(function () {
         const element = document.createElement("div");
         const row = Math.floor(scale(note.midi, 55, 72, 1, 18));
-        console.log("row: " + Math.floor(scale(note.midi, 55, 72, 1, 18)));
         element.style.gridArea = `1 / ${row}/ 19 / ${row}`;
 
         const hue = scale(note.midi, 55, 72, 0, 70);
@@ -143,8 +155,6 @@ async function draw() {
 // start transport
 function start() {
   document.body.removeChild(startButton);
-  document.body.appendChild(containerGrid);
-  containerGrid.appendChild(gridTwo);
-  containerGrid.appendChild(gridOne);
+
   Transport.start();
 }
