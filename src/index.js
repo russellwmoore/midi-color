10819f583b707b6867b1684b08dfb67eb5a095d1
Russell Moore <russellwmoore@gmail.com>
2020-05-25 13:57:11 -0400

    Getting to know the gal

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import { Tone } from "tone";
-import { Midi } from "@tonejs/midi";
-import { Player } from "tone";
-import { Buffer } from "tone";
-import { Draw } from "tone";
-import { Transport } from "tone";
+import { Tone } from 'tone';
+import { Midi } from '@tonejs/midi';
+import { Player } from 'tone';
+import { Buffer } from 'tone';
+import { Draw } from 'tone';
+import { Transport } from 'tone';
 // import UnmuteButton from "unmute";
-import mobile from "is-mobile";
+import mobile from 'is-mobile';
 
-const height = mobile() ? window.innerHeight + "px" : "100vh";
+const height = mobile() ? window.innerHeight + 'px' : '100vh';
 
 // styles
 const buttonStyle = `
@@ -44,8 +44,8 @@ const innerGridStyle = `
   display: grid;
   height: 100%;
   width: 100%;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
-  grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  grid-template-columns: repeat(18, 1fr);
+  grid-template-rows: repeat(18, 1fr);
   box-sizing: border-box;
 `;
 
@@ -56,33 +56,34 @@ const containerGridStyle = `
   align-items: center;
 
   grid-template-columns: 1fr;
-  grid-template-rows: 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
   box-sizing: border-box;
-  
 `;
 
+// ideally note spread covers range of current 'piece'
+
 // create start button
-const startButton = document.createElement("div");
+const startButton = document.createElement('div');
 startButton.style = buttonStyle;
-startButton.addEventListener("click", start);
-startButton.setAttribute("id", "start");
+startButton.addEventListener('click', start);
+startButton.setAttribute('id', 'start');
 
-const replayButton = document.createElement("div");
+const replayButton = document.createElement('div');
 replayButton.style = buttonStyle;
-replayButton.addEventListener("click", replay);
-replayButton.setAttribute("id", "replay");
+replayButton.addEventListener('click', replay);
+replayButton.setAttribute('id', 'replay');
 
-const startText = document.createElement("span");
+const startText = document.createElement('span');
 startText.style = messageStyle;
-startText.innerText = "start";
+startText.innerText = 'start';
 
-const replayText = document.createElement("span");
+const replayText = document.createElement('span');
 replayText.style = messageStyle;
-replayText.innerText = "play again";
+replayText.innerText = 'play again';
 
-const soundMessage = document.createElement("span");
+const soundMessage = document.createElement('span');
 soundMessage.style = messageStyleSmall;
-soundMessage.innerText = "(turn on sound)";
+soundMessage.innerText = '(turn on sound)';
 
 // apply button styles
 startButton.appendChild(startText);
@@ -90,23 +91,29 @@ startButton.appendChild(soundMessage);
 replayButton.appendChild(replayText);
 
 // create grids
-const appContainer = document.createElement("div");
-appContainer.setAttribute("id", "appContainer");
+const appContainer = document.createElement('div');
+appContainer.setAttribute('id', 'appContainer');
 
-const containerGrid = document.createElement("div");
-appContainer.setAttribute("id", "containerGrid");
+const containerGrid = document.createElement('div');
+appContainer.setAttribute('id', 'containerGrid');
 
-const gridOne = document.createElement("div");
-appContainer.setAttribute("id", "gridOne");
+// const gridOne = document.createElement('div'); // TODO: make function that creates  to dynamically make these along with how many midi tracks there are
+// gridOne.setAttribute('id', 'gridOne');
 
-const gridTwo = document.createElement("div");
-appContainer.setAttribute("id", "gridTwo");
+const makeGridDiv = (id) => {
+  const div = document.createElement('div');
+  div.setAttribute('id', `grid-${id}`);
+  return div;
+};
+// const gridTwo = document.createElement('div');
+// gridTwo.setAttribute('id', 'gridTwo');
 
+let containerDivs = [];
 // apply styles
 appContainer.style = appContainerStyle;
 containerGrid.style = containerGridStyle;
-gridOne.style = innerGridStyle;
-gridTwo.style = innerGridStyle;
+// gridOne.style = innerGridStyle;
+// gridTwo.style = innerGridStyle;
 
 // append elements
 document.body.appendChild(appContainer);
@@ -114,15 +121,19 @@ appContainer.appendChild(startButton);
 
 // create Tone.Player
 const player = new Player({
-  url: "./audio/bachAudio.mp3",
+  url: './audio/bachAudio.mp3',
   fadeIn: 0,
   fadeOut: 0.1,
 }).toMaster();
 player.sync().start();
 
 // on audio load, schedule drawing
-Buffer.on("load", () => {
-  draw();
+Buffer.on('load', () => {
+  draw([
+    './midi/bachChords.mid',
+    './midi/bachKeys2.mid',
+    './midi/bachKeys1.mid',
+  ]); // TODO: draw needs to take in midi URLs. Where should theys get them? Also need to make enough grids for each midi track.
 });
 
 //-----------------------------------------------------------------------
@@ -141,63 +152,103 @@ function scale(value, inMin, inMax, outMin, outMax) {
 
 //-----------------------------------------------------------------------
 // schedule drawing based on midi note times
-async function draw() {
+async function draw(midiURLs) {
+  // TODO: amount of grids is determined by number of midi tracks. Could use array indexes here.
   // get midi data
-  const keysOne = await Midi.fromUrl("./midi/bachKeys1.mid");
-  const keysTwo = await Midi.fromUrl("./midi/bachKeys2.mid");
-
+  const allKeys = await Promise.all(
+    midiURLs.map((midiURL) => Midi.fromUrl(midiURL))
+  );
+  console.log('allkeys', allKeys);
+  // const keysOne = await Midi.fromUrl('./midi/bachKeys1.mid');
+  // const keysTwo = await Midi.fromUrl('./midi/bachKeys2.mid');
+  // const keysThree = await Midi.fromUrl('./midi/keys.mid');
+  // console.log(keysTwo);
   // get notes from first track
-  const keysOneNotes = keysOne.tracks[0].notes;
-  const keysTwoNotes = keysTwo.tracks[0].notes;
+  // const keysOneNotes = keysOne.tracks[0].notes;
+  // const keysTwoNotes = keysTwo.tracks[0].notes;
 
-  //loop through keysOneNotes
-  keysOneNotes.forEach((note) => {
-    let time = note.time;
-    let duration = note.duration;
-    // schedule append element based on note.time
-    Transport.schedule(function (time) {
-      Draw.schedule(function () {
-        const element = document.createElement("div");
-        const row = Math.floor(scale(note.midi, 55, 72, 1, 18));
-        element.style.gridArea = `1 / ${row}/ 19 / ${row}`;
-
-        const hue = scale(note.midi, 55, 72, 240, 300);
-        const saturation = 100;
-        const lightness = scale(note.midi, 55, 72, 40, 50);
-        element.style.background = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
-        gridOne.appendChild(element);
-
-        // schedule remove element after note duration
-        Draw.schedule(function () {
-          gridOne.removeChild(element);
-        }, time + duration + 0.05);
-      }, time);
-    }, time);
+  // make as many containers as we have tracks
+  containerDivs = allKeys.map((singleKey, idx) => {
+    return makeGridDiv(idx);
   });
 
-  keysTwoNotes.forEach((note) => {
-    let time = note.time;
-    let duration = note.duration;
-    // schedule append element based on note.time
-    Transport.schedule(function (time) {
-      Draw.schedule(function () {
-        const element = document.createElement("div");
-        const row = Math.floor(scale(note.midi, 55, 72, 1, 18));
-        element.style.gridArea = `1 / ${row}/ 19 / ${row}`;
-
-        const hue = scale(note.midi, 55, 72, 0, 70);
-        const saturation = 100;
-        const lightness = scale(note.midi, 55, 72, 40, 50);
-        element.style.background = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
-        gridTwo.appendChild(element);
-
-        // schedule remove element after note duration
+  allKeys.forEach((midiTrack, idx) => {
+    console.log('mTrack', midiTrack, idx);
+    midiTrack.tracks[0].notes.forEach((note) => {
+      let time = note.time;
+      let duration = note.duration;
+      // schedule append element based on note.time
+      Transport.schedule(function (time) {
         Draw.schedule(function () {
-          gridTwo.removeChild(element);
-        }, time + duration + 0.05);
+          const element = document.createElement('div');
+          const col = Math.floor(scale(note.midi, 55, 72, 1, 18));
+          element.style.gridArea = `1 / ${col}/ 19 / ${col}`;
+
+          const hue = scale(note.midi, 55, 72, 240, 300);
+          const saturation = 100;
+          const lightness = scale(note.midi, 55, 72, 40, 50);
+          element.style.background = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+          console.log(containerDivs[idx]);
+          containerDivs[idx].appendChild(element);
+
+          // schedule remove element after note duration
+          Draw.schedule(function () {
+            containerDivs[idx].removeChild(element);
+          }, time + duration + 0.05);
+        }, time);
       }, time);
-    }, time);
+    });
   });
+  console.log(keysOne);
+
+  // loop through keysOneNotes
+  // keysOneNotes.forEach((note) => {
+  //   let time = note.time;
+  //   let duration = note.duration;
+  //   // schedule append element based on note.time
+  //   Transport.schedule(function (time) {
+  //     Draw.schedule(function () {
+  //       const element = document.createElement('div');
+  //       const row = Math.floor(scale(note.midi, 55, 72, 1, 18));
+  //       element.style.gridArea = `${Math.floor(Math.random()* 19)} / ${row}/ ${Math.floor(Math.random()* 19)} / ${row}`;
+
+  //       const hue = scale(note.midi, 55, 72, 240, 300);
+  //       const saturation = 100;
+  //       const lightness = scale(note.midi, 55, 72, 40, 50);
+  //       element.style.background = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+  //       gridOne.appendChild(element);
+
+  //       // schedule remove element after note duration
+  //       Draw.schedule(function () {
+  //         gridOne.removeChild(element);
+  //       }, time + duration + 0.05);
+  //     }, time);
+  //   }, time);
+  // });
+
+  // keysTwoNotes.forEach((note) => {
+  //   let time = note.time;
+  //   let duration = note.duration;
+  //   // schedule append element based on note.time
+  //   Transport.schedule(function (time) {
+  //     Draw.schedule(function () {
+  //       const element = document.createElement('div');
+  //       const row = Math.floor(scale(note.midi, 55, 72, 1, 18));
+  //       element.style.gridArea = `1 / ${row}/ 19 / ${row}`;
+
+  //       const hue = scale(note.midi, 55, 72, 0, 70);
+  //       const saturation = 100;
+  //       const lightness = scale(note.midi, 55, 72, 40, 50);
+  //       element.style.background = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+  //       gridTwo.appendChild(element);
+
+  //       // schedule remove element after note duration
+  //       Draw.schedule(function () {
+  //         gridTwo.removeChild(element);
+  //       }, time + duration + 0.05);
+  //     }, time);
+  //   }, time);
+  // });
 }
 
 //-----------------------------------------------------------------------
@@ -206,25 +257,35 @@ function replay() {
   Transport.position = 0;
   appContainer.removeChild(replayButton);
   appContainer.appendChild(containerGrid);
-  containerGrid.appendChild(gridTwo);
-  containerGrid.appendChild(gridOne);
+  // containerGrid.appendChild(gridTwo);
+  // containerGrid.appendChild(gridOne);
+  containerDivs.forEach((cd) => {
+    cd.style = innerGridStyle;
+    containerGrid.appendChild(cd);
+  });
   // Player.seek(0);
+  // Transport.bpm.value = 60;
   Transport.start();
 }
 
 //-----------------------------------------------------------------------
 // start transport
 function start() {
-  const unmuteButton = document.querySelector("#unmute-button");
+  const unmuteButton = document.querySelector('#unmute-button');
 
   //unmuteButton.click();
   appContainer.removeChild(startButton);
   appContainer.appendChild(containerGrid);
-  containerGrid.appendChild(gridTwo);
-  containerGrid.appendChild(gridOne);
+  // create with the correct varaibles the container and the grids
+  containerDivs.forEach((cd) => {
+    cd.style = innerGridStyle;
+    containerGrid.appendChild(cd);
+  });
+  // containerGrid.appendChild(gridTwo);
+  // containerGrid.appendChild(gridOne);
 
   setTimeout(() => {
-    console.log("done");
+    console.log('done');
     appContainer.removeChild(containerGrid);
     appContainer.appendChild(replayButton);
   }, player.buffer.duration * 1000);
@@ -235,10 +296,12 @@ function start() {
   // Transport.bpm.value = 114;
   // Transport.loopStart = 0;
   // Transport.loopEnd = player.buffer.duration;
-  const pressed = unmuteButton.getAttribute("aria-pressed");
+  const pressed = unmuteButton.getAttribute('aria-pressed');
   if (mobile({ tablet: true })) {
     unmuteButton.click();
   }
+  // Transport.bpm.value = 10;
+
   Transport.start();
 
   document.body.removeChild(unmuteButton);
