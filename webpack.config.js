b988f70c4cd76445f52a5bfe644bce6231fefb57
Russell Moore <russellwmoore@gmail.com>
2020-08-12 14:30:22 -0400

    get webpack reload working

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   },
-  mode: "development", // change to expression based on env variables
-  devtool: "source-map", // git ignore everything in dist
+  mode: 'development', // change to expression based on env variables
+  devtool: 'source-map', // git ignore everything in dist
+  devServer: {
+    contentBase: path.join(__dirname),
+  },
   module: {
-    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }],
+    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }],
   },
 };
